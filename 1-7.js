4392b16544f19b8a27586343b564275838c03c97
Eduardo.EzTr <142454612+EduardoET15@users.noreply.github.com>
2025-03-02 17:57:58 -0600

    Add files via upload

---
+++ 1-7.js
@@ -0,0 +1,33 @@
+let numeroUn = 1
+let stringUn = '1'
+let numeroTreinta = 30
+let stringTreinta = '30'
+let numeroDiez = 10
+let stringDiez = '10'
+
+if (numeroUn === stringUn) {
+    console.log('Las variables numeroUn y stringUn tienen el mismo valor, pero tipos diferentes');
+} else if (numeroUn == stringUn) {
+    console.log('Las variables numeroUn y stringUn tienen el mismo tipo pero diferente valor');
+} else {
+    console.log("Las variables numeroUn y strigUN no tienen el mismo valor");
+}
+
+if (numeroTreinta === stringTreinta) {
+    console.log('Las variables numeroTreinta y stringTreinta tienen el mismo valor y el mismo tipo');
+} else if (numeroTreinta == stringTreinta) {
+    console.log('Las variables numeroTreinta y stringTreinta tienen el mismo tipo pero diferente valor');
+}
+else {
+    console.log('Las variables numeroTreinta y stringTreinta no tienen el mismo tipo');
+}
+
+
+if (numeroDiez === stringDiez) {
+    console.log('Las variables numeroDiez y stringDiez tienen el mismo valor, pero tipos diferentes');
+} else if (numeroDiez == stringDiez) {
+    console.log('Las variables numeroDiez y stringDiez tienen el mismo tipo pero diferente valor');
+} else {
+    numeroDiez != stringDiez;
+    console.log('Las variables numeroDiez y stringDiez no tienen el mismo valor');
+}
